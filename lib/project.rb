778807021fc4e2af48ade8acb7b6802ac9b082a4
James Rogers <jarogers095@gmail.com>
2020-02-13 06:39:32 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,13 @@
 class Project
   @@all = []
-  attr_reader(:name)
+  attr_reader(:title, :backers)
   
-  def initialize(project_name)
-    @name = project_name
+  def initialize(project_title)
+    @title = project_title
+    @backers = []
   end
+  
+  
   
   def self.all()
     return @@all
