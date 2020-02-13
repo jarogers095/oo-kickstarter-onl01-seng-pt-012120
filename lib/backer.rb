23b9b2a79ffd67046b02991badd21c2cf26ab4d7
James Rogers <jarogers095@gmail.com>
2020-02-13 06:38:11 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,10 @@
 class Backer
   @@all = []
-  attr_accessor(:name)
+  attr_accessor(:name, :backed_projects)
   
   def initialize(backer_name)
     @name = backer_name
+    @backed_projects = []
   end
   
   def self.all()
